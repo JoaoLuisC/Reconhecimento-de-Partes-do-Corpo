90f9ff15e5e0f9aba25dbb1d4646a9899881f162
joao luis <joao.lc12@hotmail.com>
2025-03-27 14:23:50 -0300

    teste de camera

--- script.js
+++ script.js
@@ -25,10 +25,12 @@ async function initWebcam() {
     webcam = new tmImage.Webcam(300, 300, flip);
     await webcam.setup();
     await webcam.play();
-    window.requestAnimationFrame(loop);
 
     document.getElementById("webcam-container").innerHTML = "";
     document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+    // Atualizar predição a cada 5 segundos
+    setInterval(predictWebcam, 5000);
 }
 
 async function loop() {
